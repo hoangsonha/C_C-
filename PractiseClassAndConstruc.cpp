8c175c09b893c637ef79fa8c3ca930334179f960
hoangsonha <hahsse160429@fpt.edu.vn>
2024-08-01 11:04:15 +0700

    practise class with enum, union

--- PractiseClassAndConstruc.cpp
+++ PractiseClassAndConstruc.cpp
@@ -3,39 +3,142 @@
 
 using namespace std;
 
-struct MyMovie
+enum Sex
+{
+    MALE,
+    FEMALE
+};
+
+string EnumToString(Sex sex)
+{
+    if (sex == Sex::FEMALE)
+        return "Female";
+    else if (sex == Sex::MALE)
+        return "Male";
+    return nullptr;
+};
+
+class Person
 {
 private:
     string name;
     int age;
+    double weight;
+    bool active;
 
 public:
-    MyMovie(string name, int age) : name(name), age(age) {}
-    ~MyMovie()
+    Person(string name, int age, double weight, bool active = true) : name(name), age(age), weight(weight), active(active) {}
+    ~Person()
     {
-        cout << "\nDa huy vung nho\n";
+        cout << "Da huy vung nho!!!" << endl;
+    }
+    virtual void sleep()
+    {
+        cout << name << " is sleeping" << endl;
+    }
+    string getName()
+    {
+        return name;
     }
     int getAge()
     {
         return age;
     }
-    void setAge(MyMovie *myMovie)
+    bool getActive() { return active; }
+    double getWeight() { return weight; }
+    void toString()
     {
-        (*myMovie).age = 18;
+        cout << name << ", " << age << ", " << weight << ", " << active << endl;
     }
 };
 
+class Man : public Person
+{
+private:
+    Sex sex = Sex::MALE;
+
+public:
+    Man(string name, int age, double weight, bool active = true) : Person(name, age, weight, active) {};
+    void sleep()
+    {
+        cout << getName() << " is sleeping with " << EnumToString(sex) << endl;
+    }
+    void toString()
+    {
+        cout << getName() << ", " << to_string(getAge()) << ", " << to_string(getWeight()) << ", " << to_string(getActive()) << ", " << EnumToString(sex) << endl;
+    }
+    void football()
+    {
+        cout << getName() << " is playing football" << endl;
+    }
+};
+
+class Woman : public Person
+{
+private:
+    Sex sex = Sex::FEMALE;
+
+public:
+    Woman(string name, int age, double weight, bool active = true) : Person(name, age, weight, active) {};
+    void sleep()
+    {
+        cout << getName() << " is sleeping with " << EnumToString(sex) << endl;
+    }
+    void toString()
+    {
+        cout << getName() << ", " << to_string(getAge()) << ", " << to_string(getWeight()) << ", " << to_string(getActive()) << ", " << EnumToString(sex) << endl;
+    }
+    void yoga()
+    {
+        cout << getName() << " is playing yoga" << endl;
+    }
+};
+
+union Test // union là tất cả các biến nằm trung 1 chỗ -> thay đổi 1 cái sẽ ảnh hưởng tới tất cả biến khác theo giá trị biến thay đổi
+{
+
+    bool s;
+    double d;
+    int i;
+    int b;
+} so;
+
+// so.s = true;
+
+// cout << so.s << ", " << so.b << ", " << so.i << ", " << so.d << endl;
+
+// so.i = 10;
+
+// cout << so.s << ", " << so.b << ", " << so.i << ", " << so.d << endl;
+
 int main()
 {
-    struct MyMovie my1 = {"Hoang Son Ha", 21};
 
-    struct MyMovie *my2 = &my1;
+    Person *man = new Man("Hoang Son Ha", 23, 59.5);
 
-    (*my2).setAge(my2);
+    (*man).toString();
+    (*man).sleep();
 
-    cout << "my1 : " << my1.getAge() << endl;
+    Woman *woman = new Woman("Nguyen Thi Kim Ngan", 20, 45.5);
 
-    cout << "my2 : " << (*my2).getAge() << endl;
+    (*woman).toString();
+
+    delete man;
+
+    delete woman;
+
+    man = nullptr;
+
+    (*man).toString();
+
+    woman = nullptr;
+
+    (*woman).toString();
+
+    // if (sextoy == Sex::FEMALE)
+    //     cout << "Female" << endl;
+    // else if (sextoy == Sex::MALE)
+    //     cout << "Male" << endl;
 
     return 0;
 }
